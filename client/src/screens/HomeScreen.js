ce1b310d5eff7cc26cb0e84b642c799e79be7117
Mohamed Ibrahim <mohamed.ibrahim1@uoit.net>
2019-02-02 21:19:24 -0500

    post request to twilio and heroku api

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import { View, Text, Dimensions } from 'react-native';
 
 class HomeScreen extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Welcome to McBarz!</Text>
-            </View>
-        );
-    }
+
+  componentWillMount() {
+    console.log("hello world")
+    const url = 'https://mcbarzapi.herokuapp.com/send';
+    const data = {name: "mohamed", number: "+16478688461"};
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: "name=mohamed&number=+16478688461",
+    }).then(res => console.log(res))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to McBarz!</Text>
+      </View>
+      );
+  }
 }
 
 export default HomeScreen;
